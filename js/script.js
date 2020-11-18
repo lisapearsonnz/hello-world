4f214fce4c2695d9c47f856f76151a4ab37dc228
lisapearson.nz <lisapearson.nz@gmail.com>
2020-11-18 18:48:15 +1300

    javascript practice

---
+++ js/script.js
@@ -0,0 +1,31 @@
+//Javascript Playground
+
+function buttonFunction() {
+	document.getElementById("message").innerHTML = "Javascript test #2";
+}
+
+
+
+//////////////////////////////////////////////////
+
+
+				//JAVASCRIPT NOTES//
+
+
+//////////////////////////////////////////////////
+
+
+//Remember javaScript is case sensitive.
+//document.write() should only be used for testing.
+//Good practice is to put spaces around operators.
+//Best readability, its best to avoid going over 80 characters per line.
+//If it does not fit on one line, best place to break it is after an operator.
+//return exits a function
+//Variables are used to store data values
+//Expression is a combination of values, variables and operators which computes to a value.
+//let and const are new, and work similar to var
+
+// <---This is for one line of comment 
+
+/* <--This is used to write
+ more than one line of comment-->*/
